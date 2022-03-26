e28f1be8737dfd3c5c0c97686b08028d234e73ba
mitschix <23629789+mitschix@users.noreply.github.com>
2022-03-26 18:40:44 +0100

    skip impatient import if not installed

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
 -- speedsup start of lua
-require('impatient')
+-- check if impatient import works to skip if it fails
+local module_status_ok, _ = pcall(require, "impatient")
+if not module_status_ok then
+    print("impatient not found.")
+  return
+end
