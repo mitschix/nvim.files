3e726368add15935ca1d1a773f842cd9504f926a
mitschix <23629789+mitschix@users.noreply.github.com>
2022-09-15 23:13:10 +0200

    disable unused providers

--- init.lua
+++ init.lua
@@ -5,3 +5,8 @@ if not module_status_ok then
     print("impatient not found.")
   return
 end
+
+-- https://github.com/ibhagwan/nvim-lua/blob/main/lua/init.lua
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
