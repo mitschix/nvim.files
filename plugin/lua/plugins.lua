3f23c87cd21ea767e2821d65e9de378850c0ca37
mitschix <23629789+mitschix@users.noreply.github.com>
2023-09-04 00:13:30 +0200

    remove teal because of weird behaviour after switch

--- plugin/lua/plugins.lua
+++ plugin/lua/plugins.lua
@@ -1,7 +1,7 @@
 -- setup and configure minor plugins
 -- treesitter config {{{
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"bash","beancount","bibtex","c","c_sharp","cmake","comment","commonlisp","cooklang","cpp","css","dockerfile","go","gomod","gowork","graphql","hjson","hocon","html","http","java","javascript","jsdoc","json","json5","jsonc","julia","kotlin","lalrpop","latex","ledger","llvm","lua","make","ninja","nix","norg","pascal","perl","php","python","ql","query","r","regex","rego","rst","ruby","rust","scheme","scss","solidity","sparql","svelte","teal","toml","tsx","typescript","vim","yaml"
+  ensure_installed = {"bash","beancount","bibtex","c","c_sharp","cmake","comment","commonlisp","cooklang","cpp","css","dockerfile","go","gomod","gowork","graphql","hjson","hocon","html","http","java","javascript","jsdoc","json","json5","jsonc","julia","kotlin","lalrpop","latex","ledger","llvm","lua","make","ninja","nix","norg","pascal","perl","php","python","ql","query","r","regex","rego","rst","ruby","rust","scheme","scss","solidity","sparql","svelte","toml","tsx","typescript","vim","yaml"
 },
   highlight = {
     enable = true
