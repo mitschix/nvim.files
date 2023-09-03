0ee17affaa0f21161a36f14b0a8f8727d3542c46
mitschix <23629789+mitschix@users.noreply.github.com>
2023-09-04 00:16:07 +0200

    remove impatient (it is built into neovim afaik); set termguicolors for lazy

--- init.lua
+++ init.lua
@@ -1,12 +1,6 @@
--- speedsup start of lua
--- check if impatient import works to skip if it fails
-local module_status_ok, _ = pcall(require, "impatient")
-if not module_status_ok then
-    print("impatient not found.")
-  return
-end
-
 -- https://github.com/ibhagwan/nvim-lua/blob/main/lua/init.lua
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_node_provider = 0
+
+vim.opt.termguicolors = true
