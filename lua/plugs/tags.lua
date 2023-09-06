b4a47a07b9d42acaa1fb320bbe9c0909925a384d
mitschix <23629789+mitschix@users.noreply.github.com>
2023-09-06 22:52:22 +0200

    lazy load gutentags

--- lua/plugs/tags.lua
+++ lua/plugs/tags.lua
@@ -2,12 +2,15 @@ return {
     {'preservim/tagbar', lazy=true,
     keys={{'<F4>','<cmd>TagbarToggle<CR>', ''}}, init = function() vim.g.tagbar_autofocus = 1 end} ,
 
-    {'ludovicchabant/vim-gutentags', init = function ()
+    {'ludovicchabant/vim-gutentags',
+    event = { "BufReadPre", "BufNewFile" },
+    init = function ()
             --  vim gutentags settings
             vim.g.gutentags_add_default_project_roots = 0
             vim.g.gutentags_project_root = {'requirements.txt', '.git'}
             vim.g.gutentags_ctags_extra_args = {'--tag-relative=yes', '--fields=+ailmnS'}
             -- TODO does not work with lua?
             vim.cmd("let g:gutentags_cache_dir = stdpath('data').'//tags'")
-        end},
+        end
+    },
 }
