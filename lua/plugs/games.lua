68afda9573a0ec0dab73f42ee03a958499843aa0
mitschix <23629789+mitschix@users.noreply.github.com>
2023-09-18 00:26:44 +0200

    add "useful" stuff

--- lua/plugs/games.lua
+++ lua/plugs/games.lua
@@ -5,4 +5,7 @@ return {
         vim.keymap.set('n', '<leader>dd', function() require("duck").hatch("🦆", 5) end, {})
         vim.keymap.set('n', '<leader>dk', function() require("duck").cook() end, {})
     end},
+    { 'eandrju/cellular-automaton.nvim', cmd = 'CellularAutomaton',
+        keys = { { '<leader>fml', '<cmd>CellularAutomaton make_it_rain<CR>'},},
+    }
 }
