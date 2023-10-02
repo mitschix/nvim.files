c90e8031b056e6b076df17354f57dd387e818f96
mitschix <23629789+mitschix@users.noreply.github.com>
2023-10-03 01:08:03 +0200

    add formatting plugin to implement autoformatting for different ft

---
+++ lua/plugs/formatting.lua
@@ -0,0 +1,27 @@
+return {
+	"stevearc/conform.nvim",
+	lazy = true,
+	event = { "BufWritePre" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				bash = { "shfmt" },
+				sh = { "shfmt" },
+				-- go = { "gofmt" }, -- gopls has formatting?
+				lua = { "stylua" },
+				markdown = { "markdown-toc", "mdformat" },
+				python = { "isort", "black" }, -- replaces pylsp formatting (with autopep8/yapf)
+				tex = { "latexindent" },
+				yaml = { "yamlfix" },
+				["*"] = { "codespell" },
+				["_"] = { "trim_whitespace", "squeeze_blanks" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				timeout_ms = 500,
+			},
+		})
+	end,
+}
