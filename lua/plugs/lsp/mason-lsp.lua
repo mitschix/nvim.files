4fcb4e69cf5c2c36c25d9f39cc00e1fc2fc7fa12
mitschix <23629789+mitschix@users.noreply.github.com>
2023-10-04 00:10:01 +0200

    add auto tool installer for mason + add formatters

--- lua/plugs/lsp/mason-lsp.lua
+++ lua/plugs/lsp/mason-lsp.lua
@@ -1,6 +1,7 @@
 return {
     {
         'williamboman/mason.nvim',
+        dependencies = { 'WhoIsSethDaniel/mason-tool-installer.nvim' },
         lazy = true,
         cmd = { 'Mason' },
         config = function()
@@ -11,18 +12,18 @@ return {
             -- LSP diagnostics color config {{{
             -- TODO highlight currently not in vim.api
             vim.cmd([[
-        hi DiagnosticDefaultError ctermfg=red guifg=#D54E53
-        hi DiagnosticVirtualTextError ctermfg=red  guifg=#D54E53
+                hi DiagnosticDefaultError ctermfg=red guifg=#D54E53
+                hi DiagnosticVirtualTextError ctermfg=red  guifg=#D54E53
 
-        hi DiagnosticDefaultWarn ctermfg=yellow guifg=#E7C547
-        hi DiagnosticVirtualTextWarn ctermfg=yellow guifg=#E7C547
+                hi DiagnosticDefaultWarn ctermfg=yellow guifg=#E7C547
+                hi DiagnosticVirtualTextWarn ctermfg=yellow guifg=#E7C547
 
-        hi DiagnosticDefaultInfo ctermfg=white guifg=#EAEAEA
-        hi DiagnosticVirtualTextInfo ctermfg=white guifg=#EAEAEA
+                hi DiagnosticDefaultInfo ctermfg=white guifg=#EAEAEA
+                hi DiagnosticVirtualTextInfo ctermfg=white guifg=#EAEAEA
 
-        hi DiagnosticDefaultHint ctermfg=blue guifg=#7AA6DA
-        hi DiagnosticVirtualTextHint ctermfg=blue guifg=#7AA6DA
-        ]])
+                hi DiagnosticDefaultHint ctermfg=blue guifg=#7AA6DA
+                hi DiagnosticVirtualTextHint ctermfg=blue guifg=#7AA6DA
+            ]])
             -- }}}
 
             -- remove diagnostic signs and only color numbers
@@ -30,10 +31,27 @@ return {
             vim.fn.sign_define('DiagnosticSignWarn', { text = '', numhl = 'DiagnosticDefaultWarn' })
             vim.fn.sign_define('DiagnosticSignInfo', { text = '', numhl = 'DiagnosticDefaultInfo' })
             vim.fn.sign_define('DiagnosticSignHint', { text = '', numhl = 'DiagnosticDefaultHint' })
+
+            local mason_tool_installer = require('mason-tool-installer')
+            mason_tool_installer.setup({
+                ensure_installed = {
+                    -- formatters (shell, lua, python, markdown, latex, yaml, spelling)
+                    'shfmt',
+                    'stylua',
+                    'isort',
+                    'black',
+                    'markdown-toc',
+                    'mdformat',
+                    'latexindent',
+                    'yamlfix',
+                    -- linters
+                    'codespell', -- is used by formatter but found at linters
+                },
+            })
         end,
     },
 
-    { 'ray-x/lsp_signature.nvim', lazy = true }, -- has no trigger -> triggerd on lsp attach
+    { 'ray-x/lsp_signature.nvim', lazy = true }, -- has no trigger -> triggered on lsp attach
     { 'tami5/lspsaga.nvim', cmd = { 'Lspsaga' }, lazy = true },
 
     -- show (lsp) diagnostics msg on the upper right corner
