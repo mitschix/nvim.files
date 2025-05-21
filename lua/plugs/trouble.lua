12efdb945fe6811ffad58bf21b1865f18aac29f8
mitschix <23629789+mitschix@users.noreply.github.com>
2025-05-22 00:07:30 +0200

    fix trouble keymaps and cmd

--- lua/plugs/trouble.lua
+++ lua/plugs/trouble.lua
@@ -4,29 +4,51 @@ return {
     {
         'folke/trouble.nvim',
         lazy = true,
-        keys = { '<leader>xx' },
-        cmd = { 'TroubleToggle' },
+        cmd = 'Trouble',
         dependencies = 'nvim-tree/nvim-web-devicons',
-        config = function()
-            require('trouble').setup({
-                position = 'right',
-                auto_preview = false,
-            })
-            -- Trouble shortcuts {{{
-            vim.keymap.set('n', '<leader>xx', '<CMD>TroubleToggle<CR>')
-            vim.keymap.set('n', '<leader>xd', '<CMD>TroubleToggle workspace_diagnostics<CR>')
-            vim.keymap.set('n', '<leader>xl', '<CMD>TroubleToggle loclist<CR>')
-            vim.keymap.set('n', '<leader>xq', '<CMD>TroubleToggle quickfix<CR>')
-            vim.keymap.set('n', 'gR', '<CMD>Trouble lsp_references<CR>')
-            -- }}}
-        end,
+        opts = {
+            win = { position = 'right' },
+            auto_preview = false,
+        },
+        keys = {
+            {
+                '<leader>xx',
+                '<cmd>Trouble diagnostics toggle<cr>',
+                desc = 'Diagnostics (Trouble)',
+            },
+            {
+                '<leader>xX',
+                '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+                desc = 'Buffer Diagnostics (Trouble)',
+            },
+            {
+                '<leader>xs',
+                '<cmd>Trouble symbols toggle focus=false<cr>',
+                desc = 'Symbols (Trouble)',
+            },
+            {
+                '<leader>xl',
+                '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+                desc = 'LSP Definitions / references / ... (Trouble)',
+            },
+            {
+                '<leader>xL',
+                '<cmd>Trouble loclist toggle<cr>',
+                desc = 'Location List (Trouble)',
+            },
+            {
+                '<leader>xQ',
+                '<cmd>Trouble qflist toggle<cr>',
+                desc = 'Quickfix List (Trouble)',
+            },
+        },
     },
     {
         'folke/todo-comments.nvim',
         dependencies = { 'nvim-lua/plenary.nvim', 'folke/trouble.nvim' },
         config = true,
         lazy = true,
-        cmd = { 'TodoTrouble' },
-        keys = { { '<leader>xt', '<CMD>TroubleToggle todo<CR>' } },
+        cmd = { 'Trouble' },
+        keys = { { '<leader>xt', '<CMD>Trouble todo<CR>' } },
     },
 }
