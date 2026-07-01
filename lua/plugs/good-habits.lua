6e1e1b3369c80072cb15bcc9e9e9123b3b23cb17
mitschix <23629789+mitschix@users.noreply.github.com>
2026-07-01 22:56:48 +0200

    refactor: keep only good habit notification in file

--- lua/plugs/good-habits.lua
+++ lua/plugs/good-habits.lua
@@ -1,16 +1,11 @@
 return {
     -- tobira watches how you actually edit. When it spots a pattern you could do better,
     -- it quietly shows you the one command that would have helped
-    'kamegoro/tobira.nvim',
-    dependencies = {
-        {
-            'rcarriga/nvim-notify',
-            init = function() vim.notify = require('notify') end,
-            opts = { background_colour = '#000000' },
+    {
+        'kamegoro/tobira.nvim',
+        event = 'VeryLazy',
+        opts = {
+            idle_delay = 5000, -- ms if inactivity
         },
-    },
-    event = 'VeryLazy',
-    opts = {
-        idle_delay = 5000, -- ms if inactivity
     },
 }
