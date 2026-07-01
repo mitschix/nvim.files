d32a9f0f815b4ccc5e20942d9ffeb51a6d8dea3d
mitschix <23629789+mitschix@users.noreply.github.com>
2026-07-01 22:58:19 +0200

    refactor: move all ui related settings to same file

--- lua/plugs/ui.lua
+++ lua/plugs/ui.lua
@@ -1,5 +1,42 @@
 return {
     {
+        'tiagovla/tokyodark.nvim',
+        priority = 1000,
+        config = function()
+            require('tokyodark').setup({ transparent_background = true, gamma = 1.2 })
+            vim.cmd('colorscheme tokyodark')
+
+            -- Overwrite defaults of tokyodark
+            vim.cmd([[
+        highlight Normal guifg=#E8E8E8
+        highlight Comment guifg=#7f8490
+        ]])
+
+            -- Highlight extra whitespaces
+            vim.cmd('highlight ExtraWhitespace ctermbg=darkblue guibg=#81A2BE')
+        end,
+    },
+    {
+        'catgoose/nvim-colorizer.lua',
+        opts = {
+            options = {
+                parsers = { css = true },
+                display = {
+                    mode = 'virtualtext',
+                    virtualtext = {
+                        position = 'before',
+                    },
+                },
+            },
+        },
+        event = { 'BufReadPre' },
+    },
+    {
+        'xiyaowong/nvim-cursorword',
+        event = { 'BufReadPre', 'BufNewFile' },
+        config = function() vim.cmd('highlight CursorWord gui=underline') end,
+    },
+    {
         'rcarriga/nvim-notify',
         init = function() vim.notify = require('notify') end,
         opts = { background_colour = '#000000' },
