d259fc059d77d635d76a6ad19d1ddc861e589956
mitschix <23629789+mitschix@users.noreply.github.com>
2026-07-01 22:20:14 +0200

    feat: add tobira for good habits

---
+++ lua/plugs/good-habits.lua
@@ -0,0 +1,16 @@
+require({
+    -- tobira watches how you actually edit. When it spots a pattern you could do better,
+    -- it quietly shows you the one command that would have helped
+    'kamegoro/tobira.nvim',
+    dependencies = {
+        {
+            'rcarriga/nvim-notify',
+            init = function() vim.notify = require('notify') end,
+            opts = { background_colour = '#000000' },
+        },
+    },
+    event = 'VeryLazy',
+    opts = {
+        idle_delay = 5000, -- ms if inactivity
+    },
+})
