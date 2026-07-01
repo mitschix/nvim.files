43ab67c08ce1c29009d5e248880fd97f856f23c8
mitschix <23629789+mitschix@users.noreply.github.com>
2026-07-01 23:55:44 +0200

    feat: add copilot llm for cmp completion and codecompanion

--- lua/plugs/llms.lua
+++ lua/plugs/llms.lua
@@ -1,17 +1,39 @@
 return {
-    'olimorris/codecompanion.nvim',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'nvim-treesitter/nvim-treesitter',
-    },
-    opts = {
-        strategies = {
-            chat = { adapter = 'ollama', model = 'deepseek-coder-v2:latest' },
-            inline = { adapter = 'ollama', model = 'deepseek-coder-v2:latest' },
-            agent = { adapter = 'ollama', model = 'deepseek-coder-v2:latest' },
+    {
+        'olimorris/codecompanion.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-treesitter/nvim-treesitter',
         },
         opts = {
-            log_level = 'DEBUG',
+            opts = {
+                log_level = 'TRACE',
+            },
+        },
+    },
+    {
+        'zbirenbaum/copilot.lua',
+        cmd = 'Copilot',
+        event = 'InsertEnter',
+        opts = {
+            filetypes = {
+                help = false,
+                gitcommit = false,
+                gitrebase = false,
+                hgcommit = false,
+                svn = false,
+                cvs = false,
+                go = true,
+                rust = true,
+                lua = true,
+                ['*'] = function()
+                    if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
+                        -- disable for .env files
+                        return false
+                    end
+                    return true
+                end,
+            },
         },
     },
 }
